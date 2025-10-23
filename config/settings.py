76fe4a279e4efd79b7a110fc6a6a9c7cb8875aaa
Kostya Chu <konstantin@mychinaroots.com>
2025-10-24 02:40:06 +0300

    add whitenoise for static

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -85,9 +86,13 @@ USE_TZ = True
 
 
 # Static files
-STATIC_URL = "static/"
+# Use absolute path for correct URL generation in production
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS: list[Path] = []
+
+# WhiteNoise for efficient static file serving in production
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
